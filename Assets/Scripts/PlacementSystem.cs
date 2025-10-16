85a5b6aaa48343db91afa82379453636326f888a
Harriet04 <harriet.mangum@gmail.com>
2025-10-16 10:48:40 -0500

    changed scaling of grid

--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlacementSystem : MonoBehaviour
 {
     [SerializeField]
-    GameObject mouseIndicator;
-
+    GameObject mouseIndicator, cellIndicator;
+    
     [SerializeField]
     private GridTracking gridtracking;
+
+    [SerializeField]
+    private Grid grid;
 
     private void Update()
     {
         Vector3 mousePosition = gridtracking.GetSelectedMapPosition();
+        Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
+        cellIndicator.transform.position = grid.CellToWorld(gridPosition);
     }
 }
