9631def9ada7e1047e73eeb068ef5205b41a8ad8
jackwcoker <jackwcoker@gmail.com>
2025-11-02 14:11:27 -0600

    Implemented Grid Snapping
    In the PlacementSystem component, used the right hand to point where a block should be placed within the grid.
    
    Compensated for grid.WorldToCell using flooring by adding half a cell so it properly rounds the position

--- Assets/Scripts/PlacementSystem.cs
+++ Assets/Scripts/PlacementSystem.cs
@@ -13,11 +13,37 @@ public class PlacementSystem : MonoBehaviour
     [SerializeField]
     private Grid grid;
 
+    [SerializeField]
+    GameObject PlacementObject, UnsnappedObject, Hand;
+
+    [SerializeField]
+    private Vector2 CastDistanceRange;
+    private float CastDistance = 0.0f;
+
+    private void Start()
+    {
+        //Set CastDistance to the minimum cast value
+        //We'll want to handle controller events to to push/pull this distance.
+        CastDistance += CastDistanceRange.x;
+    }
+
     private void Update()
     {
+        /* Cast system
         Vector3 mousePosition = gridtracking.GetSelectedMapPosition();
         Vector3Int gridPosition = grid.WorldToCell(mousePosition);
         mouseIndicator.transform.position = mousePosition;
         cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+        */
+
+        //Sphere should be roughly CastDistance away from the hand. We'll snap that position to the 3D grid.
+        //The forward vector on the hand mesh is facing backwards, that's why I'm subtracting.
+        Vector3 LoosePosition = Hand.transform.position - Hand.transform.forward*CastDistance;
+        //WorldToCell floors rather than rounds, so add half a cell
+        Vector3Int SnappedCell = grid.WorldToCell(LoosePosition+grid.cellSize*0.5f);
+        Vector3 SnappedPosition = grid.CellToWorld(SnappedCell);
+        PlacementObject.transform.position = SnappedPosition;
+        UnsnappedObject.transform.position = LoosePosition;
+
     }
 }
