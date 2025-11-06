664aa9fd6bde597b8fec5165d57c81c6be914dc4
Freeman, Jabari <46659998+MrDevus@users.noreply.github.com>
2025-11-06 00:36:30 -0600

    Button Is now Programable
    Scripts can be assigned to the Push Button under its "Interactable" Hierarchy in the "VR Button" script. Scripts to run can be set on its "onPressed" and "onReleased" components. Duplicate Button Prefab as needed.

---
+++ Assets/VRButton.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class VRButton : MonoBehaviour
+{
+    public float deadTime = 1.0f; //Time to prevent button activation after press
+
+    private bool _deadTimeActive = false; // Bool for button lock state
+
+    public UnityEvent onPressed, OnReleased;//public events for editor functions
+
+    private void OnTriggerEnter(Collider other) // checks if button has entered Pressed and sets onPressed
+    {
+        if(other.tag == "Button" && !_deadTimeActive)
+        {
+            onPressed.Invoke();
+            Debug.Log("VR Button Pressed");
+        }
+    }
+    private void OnTriggerExit(Collider other) // checks if button has entered Released and sets onReleased
+    {
+        if(other.tag=="Button" && !_deadTimeActive)
+        {
+            OnReleased?.Invoke();
+            Debug.Log("VR Button Released");
+            StartCoroutine(WaitForDeadTime());
+        }
+    }
+
+    IEnumerator WaitForDeadTime() //Locks Button Press until deadTime times out
+    {
+        _deadTimeActive = true;
+        yield return new WaitForSeconds(deadTime);
+        _deadTimeActive = false;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
