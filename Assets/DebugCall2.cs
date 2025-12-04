ddacfc34385450f20547c9da5f05294e3805593b
Freeman, Jabari <mrdvus12@gmail.com>
2025-12-03 23:23:54 -0600

    Solved assigning the buttons with different functions
    Use Ctrl+D to copy the button as an individual prefab

--- Assets/DebugCall2.cs
+++ Assets/DebugCall2.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class DebugCall2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //Debug.Log("Debug Script Called");
+        //Debug.Log("A different Debug Script was called!");
     }
-
     public void Call()
     {
-        Debug.Log("Debug Script Called");
+        Debug.Log("A different Debug Script was called!");
     }
     // Update is called once per frame
     void Update()
