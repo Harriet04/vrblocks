0102121f4affb65d4696ec606bc1a4922b8acab8
Freeman, Jabari <mrdvus12@gmail.com>
2026-02-09 09:07:35 -0600

    Attempted Set up for clearing clock list
    just comments, debug output and for now

--- Assets/Blocks/Scripts/SwitchBlockMode.cs
+++ Assets/Blocks/Scripts/SwitchBlockMode.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class SwitchBlockMode : MonoBehaviour
 {
+    //AddObject TestLink;//refrence variable to AddObject script 
     bool SwitchON = false;// boolean for detecting when the switch is active
     // Start is called before the first frame update
     void Start()
     {
-  
+       // TestLink = GameObject.FindGameObjectWithTag("AddObject").GetComponent<AddObject>();// locates existing refrence of AddObject
+    }
+
+    public void ClearList()// set clear list function here
+    {
+        Debug.Log("Clear List Called");
     }
     public void Switch()
     {
+        ClearList(); //place holder for actual clear block list function
+        //TestLink.LinkTest();//calls public function fron object
+
+
         if (SwitchON == true)// switch statment for when the switch is active
         {
             SwitchON = false; Debug.Log("Simple Mode: OFF");
