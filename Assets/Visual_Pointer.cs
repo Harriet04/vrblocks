4e92d90c212d2b8a65f49ab058abf02148441884
Freeman, Jabari <mrdvus12@gmail.com>
2026-03-29 00:25:35 -0500

    Updated Visual Pointer Text Box
    Visual Pointer Text box can now be edited with out extending child list in the side menu

--- Assets/Visual_Pointer.cs
+++ Assets/Visual_Pointer.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Visual_Pointer : MonoBehaviour
 {
     public float amplitude = 1.0f; // Maximum distance 
     public float frequency = 1.0f; // Speed of the oscillation
+    public string caption = "Enter a Caption"; // string to be read and sent to text box
 
     public Transform pointerArrow; //variable to hold secondary transform information
     public Vector3 ArrowRotation; //variable to edit global rotation in editor
     public Transform textLabel; // variable to hold text label transform information
+    TMP_Text textMesh;// variable to hold target text mesh
     // Start is called before the first frame update
     void Start()
     {
         pointerArrow.transform.rotation = Quaternion.Euler(ArrowRotation); //set arrow global rotation to ArrowRotation values.
+        textMesh = GetComponentInChildren<TMP_Text>(); //find existing TMP Text element
+        textMesh.text = caption; // set TMP Text to new text from edit mode
     }
 
     // Update is called once per frame
